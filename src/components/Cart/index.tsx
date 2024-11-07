3c85e29d40ae6c6f3d95bfa329662edee9115a7d
Marlonn Locatelli <marlonn@gmail.com>
2024-11-07 16:38:35 -0300

    ADD: estado de visibilidade do carrinho.

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,3 +1,6 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
 import Button from '../Button'
 import {
   CartContainer,
@@ -10,42 +13,52 @@ import {
 
 import Tag from '../Tag'
 import IMG from '../../assets/images/star_wars.png'
+import { close } from '../../store/reducers/cart'
 
-const Cart = () => (
-  <CartContainer>
-    <Overlay />
-    <Sidebar>
-      <ul>
-        <CartItem>
-          <img src={IMG} />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 190,90</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-        <CartItem>
-          <img src={IMG} />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 190,90</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-      </ul>
-      <Quantity>2 jogo(s) no carrinho</Quantity>
-      <Prices>
-        Total de R$ 0,00 <span>Em até 6x sem juros</span>
-      </Prices>
-      <Button type="button" title="Clique aqui para continuar com a compra">
-        Continuar com a compra
-      </Button>
-    </Sidebar>
-  </CartContainer>
-)
+const Cart = () => {
+  const { isOpen } = useSelector((state: RootReducer) => state.cart)
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <Overlay onClick={closeCart} />
+      <Sidebar>
+        <ul>
+          <CartItem>
+            <img src={IMG} />
+            <div>
+              <h3>Nome do jogo</h3>
+              <Tag>RPG</Tag>
+              <Tag>PS5</Tag>
+              <span>R$ 190,90</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+          <CartItem>
+            <img src={IMG} />
+            <div>
+              <h3>Nome do jogo</h3>
+              <Tag>RPG</Tag>
+              <Tag>PS5</Tag>
+              <span>R$ 190,90</span>
+            </div>
+            <button type="button" />
+          </CartItem>
+        </ul>
+        <Quantity>2 jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total de R$ 0,00 <span>Em até 6x sem juros</span>
+        </Prices>
+        <Button type="button" title="Clique aqui para continuar com a compra">
+          Continuar com a compra
+        </Button>
+      </Sidebar>
+    </CartContainer>
+  )
+}
 
 export default Cart
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,45 @@
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+
+import { open } from '../../store/reducers/cart'
 
 import { CartLink, HeaderBar, LinkItem, Links } from './styles'
 import logo from '../../assets/images/logo.svg'
 import cart from '../../assets/images/carrinho.svg'
 
-const Header = () => (
-  <HeaderBar>
-    <div>
-      <Link to="/">
-        <img src={logo} alt="EPlay" />
-      </Link>
-      <nav>
-        <Links>
-          <LinkItem>
-            <Link to={'/categorias'}>Categorias</Link>
-          </LinkItem>
-          <LinkItem>
-            <a href="#">Novidades</a>
-          </LinkItem>
-          <LinkItem>
-            <a href="#">Promoções</a>
-          </LinkItem>
-        </Links>
-      </nav>
-    </div>
-    <CartLink href="#">
-      0 - produto(s)
-      <img src={cart} alt="Carrinho" />
-    </CartLink>
-  </HeaderBar>
-)
+const Header = () => {
+  const dispatch = useDispatch()
+
+  const openCart = () => {
+    dispatch(open())
+  }
+
+  return (
+    <HeaderBar>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="EPlay" />
+        </Link>
+        <nav>
+          <Links>
+            <LinkItem>
+              <Link to={'/categorias'}>Categorias</Link>
+            </LinkItem>
+            <LinkItem>
+              <a href="#">Novidades</a>
+            </LinkItem>
+            <LinkItem>
+              <a href="#">Promoções</a>
+            </LinkItem>
+          </Links>
+        </nav>
+      </div>
+      <CartLink onClick={openCart}>
+        0 - produto(s)
+        <img src={cart} alt="Carrinho" />
+      </CartLink>
+    </HeaderBar>
+  )
+}
 
 export default Header
